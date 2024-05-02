790fb72bb05434d1c2a7579abb1163ca8d9f6801
Martin Wimpress <martin@wimpress.org>
2024-05-02 13:30:28 +0100

    feat: add devshell.nix

---
+++ devshell.nix
@@ -0,0 +1,51 @@
+{
+  mkShell,
+  cdrtools,
+  curl,
+  lsb-release,
+  git,
+  gnugrep,
+  gnused,
+  jq,
+  nixpkgs-fmt,
+  procps,
+  python3,
+  qemu,
+  socat,
+  swtpm,
+  unzip,
+  usbutils,
+  util-linux,
+  wget,
+  xdg-user-dirs,
+  xrandr,
+  zsync,
+}:
+mkShell {
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    cdrtools
+    curl
+    lsb-release
+    git
+    gnugrep
+    gnused
+    jq
+    nixpkgs-fmt
+    procps
+    python3
+    qemu
+    socat
+    swtpm
+    unzip
+    usbutils
+    util-linux
+    wget
+    xdg-user-dirs
+    xrandr
+    zsync
+  ];
+
+  buildInputs = [];
+}
