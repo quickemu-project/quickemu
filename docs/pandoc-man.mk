542fcbe50ec344713db1ed0cb64455e9ff306587
Phil Clifford <philip.clifford@gmail.com>
2024-05-14 13:19:41 +0100

    fix: update Makefile for better pandoc options

--- docs/pandoc-man.mk
+++ docs/pandoc-man.mk
@@ -2,7 +2,10 @@ PANDOC ?= pandoc
 
 MANSECTION ?= 1
 
-MANPAGE.md = $(PANDOC) --standalone $(PANDOCFLAGS) --to man
+PANDOC_OPTIONS=-f gfm+definition_lists --standalone
+
+
+MANPAGE.md = $(PANDOC) --standalone $(PANDOC_OPTIONS) --to man
 
 %.$(MANSECTION): %.$(MANSECTION).md
 	$(MANPAGE.md) $< -o $@
