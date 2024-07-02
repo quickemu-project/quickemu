763bc9bc4730a945ce4241b8dd4f5c045d9c679a
Martin Wimpress <martin@wimpress.org>
2024-07-02 02:23:22 +0100

    refactor: update Nix flake to be consistent with the quickgui flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Quickemu flake";
   inputs = {
     flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*.tar.gz";
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
   };
 
   outputs = {
@@ -10,28 +10,33 @@
     flake-schemas,
     nixpkgs,
   }: let
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ] (system: function nixpkgs.legacyPackages.${system});
+      # Define supported systems and a helper function for generating system-specific outputs
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ];
+
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        system = system;
+        pkgs = import nixpkgs { inherit system; };
+      });
   in {
-    # Schemas tell Nix about the structure of your flake's outputs
+    # Define schemas for the flake's outputs
     schemas = flake-schemas.schemas;
 
-    overlays.default = final: prev: {
-      quickemu = final.callPackage ./package.nix {};
-    };
+    # Define overlays for each supported system
+    overlays = forEachSupportedSystem ({pkgs, system, ...}: {
+      default = final: prev: {
+        quickemu = final.callPackage ./package.nix { };
+      };
+    });
 
-    packages = forAllSystems (pkgs: rec {
-      quickemu = pkgs.callPackage ./package.nix {};
+    # Define packages for each supported system
+    packages = forEachSupportedSystem ({pkgs, system, ...}: rec {
+      quickemu = pkgs.callPackage ./package.nix { };
       default = quickemu;
     });
 
-    devShells = forAllSystems (pkgs: {
-      default = pkgs.callPackage ./devshell.nix {};
+    # Define devShells for each supported system
+    devShells = forEachSupportedSystem ({pkgs, system, ...}: {
+      default = pkgs.callPackage ./devshell.nix { };
     });
   };
 }
