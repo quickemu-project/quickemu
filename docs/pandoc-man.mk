05a3b34be43ff0c3875744d37a3b26ea46ff3a8a
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2024-09-12 15:44:36 +0200

    docs: Simplify make rules for pandoc
    This removes a --standalone duplicate and counter-productive
    indirections like $(MANSECTION) because it prevents having more
    than one and docs/Makefile hardcodes the section anyway.

--- docs/pandoc-man.mk
+++ docs/pandoc-man.mk
@@ -1,11 +1,7 @@
 PANDOC ?= pandoc
+PANDOC_OPTIONS = -f gfm+definition_lists
 
-MANSECTION ?= 1
+.SUFFIXES: .1.md .1
 
-PANDOC_OPTIONS=-f gfm+definition_lists --standalone
-
-
-MANPAGE.md = $(PANDOC) --standalone $(PANDOC_OPTIONS) --to man
-
-%.$(MANSECTION): %.$(MANSECTION).md
-	$(MANPAGE.md) $< -o $@
+.1.md.1:
+	$(PANDOC) --standalone $(PANDOC_OPTIONS) --to man -o $@ $<
